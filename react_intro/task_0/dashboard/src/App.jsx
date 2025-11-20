1a775ecfa9efd387e05f2b891e9ded4ff3bbe336
git-alexis <alex59.billemont@gmail.com>
2025-11-20 14:27:34 +0100

    Create a basic app named dashboard using the build tool vite

---
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import holbertonLogo from './assets/holberton-logo.jpg';
+
+function App() {
+  const year = new Date().getFullYear();
+
+  return (
+    <>
+      <div className="App-header">
+        <img src={holbertonLogo} alt="holberton logo" className="App-logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      <div className="App-footer">
+        <p>Copyright {year} - holberton School</p>
+      </div>
+    </>
+  );
+}
+
+export default App
