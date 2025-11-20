93a70dae906e1af7c90e7292cb54b5c104805ca3
git-alexis <alex59.billemont@gmail.com>
2025-11-20 17:56:02 +0100

    Remove beforeEach because i can't check with checker to understed

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -2,10 +2,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App.jsx';
 
 describe('App component', () => {
-  beforeEach(() => {
-    render(<App />);
-  });
-
   test("renders 2 input elements for email and password", () => {
     render(<App />);
     const inputs = screen.getAllByRole('textbox');
