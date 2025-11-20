0e805a919be3e73a08cbbbf39ee5e3153c3ef221
git-alexis <alex59.billemont@gmail.com>
2025-11-20 17:19:51 +0100

    REmove ':'

--- react_intro/task_2/dashboard/src/App.jsx
+++ react_intro/task_2/dashboard/src/App.jsx
@@ -18,10 +18,10 @@ function App() {
       <div className="App-body">
         <p>Login to access the full dashboard</p>
 
-        <label htmlFor="email">Email:</label>
+        <label htmlFor="email">Email</label>
         <input type="email" id="email" name="email" />
 
-        <label htmlFor="password">Password:</label>
+        <label htmlFor="password">Password</label>
         <input type="password" id="password" name="password" />
 
         <button>OK</button>
