bdeda9f7903e2ae9d6be180bc1b5abd68e379e3a
git-alexis <alex59.billemont@gmail.com>
2025-11-20 14:48:35 +0100

    Add a copy of task_0 directory

---
+++ react_intro/task_1/dashboard/src/App.spec.js
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import App from './App.jsx';
+
+describe('App component', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test('renders the h1 with text "School dashboard"', () => {
+    const heading = screen.getByRole('heading', { level: 1, name: /school dashboard/i });
+    expect(heading).toBeInTheDocument();
+  });
+
+  test('renders the correct text in app-body and app-footer', () => {
+    const bodyText = screen.getByText(/login to access the full dashboard/i);
+    const footerText = screen.getByText(new RegExp(`Copyright ${new Date().getFullYear()} - holberton School`, 'i'));
+
+    expect(bodyText).toBeInTheDocument();
+    expect(footerText).toBeInTheDocument();
+  });
+
+  test('renders an image with alt text "holberton logo"', () => {
+    const logo = screen.getByAltText(/holberton logo/i);
+    expect(logo).toBeInTheDocument();
+  });
+});
