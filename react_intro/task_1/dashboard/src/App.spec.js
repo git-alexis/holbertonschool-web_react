1ce8f919b72e117e65037233ff5c782a641761fe
git-alexis <alex59.billemont@gmail.com>
2025-11-20 16:40:55 +0100

    Add Tests to check the presence of all element in form

--- react_intro/task_1/dashboard/src/App.spec.js
+++ react_intro/task_1/dashboard/src/App.spec.js
@@ -23,4 +23,21 @@ describe('App component', () => {
     const logo = screen.getByAltText(/holberton logo/i);
     expect(logo).toBeInTheDocument();
   });
+
+  test("renders 2 input elements for email and password", () => {
+    const inputs = screen.getAllByRole("textbox");
+    const passwordInput = screen.getByLabelText(/password/i);
+
+    expect(inputs.length).toBe(1); // only email should appear in textbox query
+    expect(passwordInput).toBeInTheDocument();
+  });
+
+  test("renders 2 label elements with Email and Password", () => {
+    expect(screen.getByText(/email/i)).toBeInTheDocument();
+    expect(screen.getByText(/password/i)).toBeInTheDocument();
+  });
+
+  test("renders a button with text OK", () => {
+    expect(screen.getByRole("button", { name: /ok/i })).toBeInTheDocument();
+  });
 });
