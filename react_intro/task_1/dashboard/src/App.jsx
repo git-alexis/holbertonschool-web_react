f056d6550f48163f79db3a72284a00f74a67d475
git-alexis <alex59.billemont@gmail.com>
2025-11-20 16:29:28 +0100

    Remove modification in wrong file and directory

--- react_intro/task_1/dashboard/src/App.jsx
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -17,14 +17,6 @@ function App() {
 
       <div className="App-body">
         <p>Login to access the full dashboard</p>
-
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" />
-
-        <label htmlFor="password">Password:</label>
-        <input type="password" id="password" name="password" />
-
-        <button>OK</button>
       </div>
 
       <div className="App-footer">
