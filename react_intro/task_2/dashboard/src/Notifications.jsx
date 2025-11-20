81b5b31a8efb90313b28b9b63eaff1fd73e3781d
git-alexis <alex59.billemont@gmail.com>
2025-11-20 18:38:42 +0100

    Update the Notifications with a close-button and a list that contains the items: Default priority with the text "New course available", Urgent priority with the text "New resume available" and the text "Urgent requirement - complete by EOD" returned by getLatestNotification function using dangerouslySetInnerHTML

--- react_intro/task_2/dashboard/src/Notifications.jsx
+++ react_intro/task_2/dashboard/src/Notifications.jsx
@@ -1,9 +1,39 @@
 import './Notifications.css';
+import closeIcon from './assets/close-button.png';
+import { getLatestNotification } from './utils';
 
 function Notifications() {
+  const handleClick = () => {
+    console.log("Close button has been clicked");
+  };
+
   return (
     <div className="notification-items">
+      <button
+        style={{
+		  top: 30,
+          right: 14,
+		  position: "absolute",
+          background: "transparent",
+          border: "none",
+		  cursor: "pointer"
+        }}
+        aria-label="Close"
+        onClick={handleClick}
+      >
+        <img src={closeIcon} alt="close icon" style={{ width: "1rem", height: "1rem" }} />
+      </button>
+
       <p>Here is the list of notifications</p>
+
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
     </div>
   );
 }
