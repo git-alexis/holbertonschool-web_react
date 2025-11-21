99901cb07f65cde6acc6767b2d490a2be2cc6610
git-alexis <alex59.billemont@gmail.com>
2025-11-21 17:05:15 +0100

    Refactor test

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -5,8 +5,10 @@ import userEvent from '@testing-library/user-event'
 describe("Login component", () => {
   it("renders the input elements", () => {
     render(<Login />)
-    const twoInputs = screen.getAllByRole("textbox")
-    twoInputs.map((input) => {expect(input).toBeInTheDocument()})
+    const email = screen.getByTestId("email")
+    const password = screen.getByTestId("password")
+    expect(email).toBeInTheDocument()
+    expect(password).toBeInTheDocument()
   })
 
   it("renders the labels", () => {
