3ab0c1dc65c58e777866cbf1f12189ae91fd07d9
git-alexis <alex59.billemont@gmail.com>
2025-11-21 12:23:16 +0100

    Use only toBe and change test order

--- react_intro/task_3/dashboard/src/Notifications.spec.js
+++ react_intro/task_3/dashboard/src/Notifications.spec.js
@@ -3,17 +3,16 @@ import Notifications from './Notifications.jsx';
 
 describe('Notifications component', () => {
   it("<App /> is rendered without crashing", () => {
+    shallow(<Notifications />);
+  });
+
+  it('renders 3 list items', () => {
     const wrapper = shallow(<Notifications />)
-    expect(wrapper).to.have.lengthOf(1);
+    expect(wrapper.find('li').length).toBe(3);
   });
 
   test('renders the notifications title', () => {
     const wrapper = shallow(<Notifications />)
     expect(wrapper.contains(<p>Here is the list of notifications</p>)).toBe(true);
-  });
-
-  it('renders 3 list items', () => {
-    const wrapper = shallow(<Notifications />)
-    expect(wrapper.find('li')).to.have.lengthOf(3);
   });
 });
