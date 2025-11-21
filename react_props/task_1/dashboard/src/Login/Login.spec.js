153ba625f36c3ed3cc75042705b00d6fdf5d3338
git-alexis <alex59.billemont@gmail.com>
2025-11-21 16:47:42 +0100

    Use it instead of test

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -3,11 +3,11 @@ import Login from './Login.jsx'
 import userEvent from '@testing-library/user-event'
 
 describe("Login component", () => {
-  test("renders login", () => {
+  it("renders login", () => {
     render(<Login />)
   });
 
-  test("renders the input elements", () => {
+  it("renders the input elements", () => {
     render(<Login />)
     const email = screen.getByTestId("email")
     const password = screen.getByTestId("password")
@@ -15,7 +15,7 @@ describe("Login component", () => {
     expect(password).toBeInTheDocument()
   })
 
-  test("renders the labels", () => {
+  it("renders the labels", () => {
     render(<Login />)
     const email = screen.getByText("Email")
     const password = screen.getByText("Password")
@@ -23,14 +23,14 @@ describe("Login component", () => {
     expect(password).toBeInTheDocument()
   })
 
-  test("renders the button", () => {
+  it("renders the button", () => {
     render(<Login />)
     const boutonLogin = screen.getByTestId("boutonLogin")
     expect(boutonLogin).toBeInTheDocument()
     expect(boutonLogin.textContent).toMatch("OK")
   })
 
-  test("Focus on password input if Password label is clicked", async () => {
+  it("Focus on password input if Password label is clicked", async () => {
     render(<Login />)
     const event = userEvent.setup()
     const input = screen.getByTestId("password")
