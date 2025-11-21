324ecd20fe298ecf2c1c2cac8199276de963adfe
git-alexis <alex59.billemont@gmail.com>
2025-11-21 11:42:25 +0100

    Copy task_4 directory into task_5 directory

---
+++ react_intro/task_5/dashboard/src/Notifications.jsx
@@ -0,0 +1,41 @@
+import './Notifications.css';
+import closeIcon from './assets/close-button.png';
+import { getLatestNotification } from './utils';
+
+function Notifications() {
+  const handleClick = () => {
+    console.log("Close button has been clicked");
+  };
+
+  return (
+    <div className="notification-items">
+      <p>Here is the list of notifications</p>
+
+      <button
+        style={{
+          top: 30,
+          right: 14,
+          position: "absolute",
+          background: "transparent",
+          border: "none",
+          cursor: "pointer"
+        }}
+        aria-label="Close"
+        onClick={handleClick}
+      >
+        <img src={closeIcon} alt="close icon" style={{ width: "1rem", height: "1rem" }} />
+      </button>
+
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;
