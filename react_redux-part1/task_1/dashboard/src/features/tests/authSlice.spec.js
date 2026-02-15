1022716c5224fe7baa7a17052efa17d16df9bebc
git-alexis <alex59.billemont@gmail.com>
2026-02-15 14:36:00 +0100

    Ajout de l'import mockAxios

--- react_redux-part1/task_1/dashboard/src/features/tests/authSlice.spec.js
+++ react_redux-part1/task_1/dashboard/src/features/tests/authSlice.spec.js
@@ -1,4 +1,4 @@
-//import mockAxios from "jest-mock-axios";
+import mockAxios from "jest-mock-axios";
 import authReducer, { login, logout } from "../auth/authSlice";
 
 describe("authSlice", () => {
