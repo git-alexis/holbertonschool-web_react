c813dd51469689eef67a51fd1394b56603ae26b1
git-alexis <alex59.billemont@gmail.com>
2026-02-18 17:42:37 +0100

    Suppression des export sur le variables de la config API

--- react_redux-part1/task_1/dashboard/src/features/notifications/notificationsSlice.js
+++ react_redux-part1/task_1/dashboard/src/features/notifications/notificationsSlice.js
@@ -7,8 +7,8 @@ const initialState = {
   displayDrawer: true,
 };
 
-export const API_BASE_URL = "http://localhost:5173";
-export const ENDPOINTS = {
+const API_BASE_URL = "http://localhost:5173";
+const ENDPOINTS = {
   notifications: `${API_BASE_URL}/notifications.json`,
 };
 
