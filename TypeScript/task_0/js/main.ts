a27460be756ba2f51eab354a2b71d20588f2b5ec
git-alexis <alex59.billemont@gmail.com>
2026-04-01 13:40:06 +0200

    Correction des exports du fichier crud.d.ts

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -19,7 +19,7 @@ const student2: Student = {
     location: "Los Angeles",
 };
 
-const studentsList: Array<Student> = [student1, student2];
+const studentsList: Student[] = [student1, student2];
 
 const labels: string[] = ['firstName', 'location'];
 
--- TypeScript/task_3/js/crud.d.ts
+++ TypeScript/task_3/js/crud.d.ts
@@ -1,13 +1,7 @@
-import { RowElement, RowID } from './interface';
+import { RowID, RowElement } from "./interface";
 
-type insertRow = (row: RowElement) => number;
+export function insertRow(row: RowElement): RowID;
 
-type deleteRow = (rowId: RowID) => void;
+export function deleteRow(rowId: RowID): void;
 
-type updateRow = (rowId: RowID, row: RowElement) => number;
-
-export {
-    insertRow,
-    deleteRow,
-    updateRow,
-}
+export function updateRow(rowId: RowID, row: RowElement): RowID;
